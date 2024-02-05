2fd6068eb402338a7df5600b1fd39cbb7c6f7a31
bunnana <na.nuttanont@gmail.com>
2024-02-06 02:27:55 +0700

    add jwt middleware to app

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cookieParser from "cookie-parser";
 import { config } from "dotenv";
 import express from "express";
 import "express-async-errors";
@@ -8,6 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(jwtMiddleware.decodeJWT);
 
 app.get("/", (req, res) => {
 	res.status(200).send("Server is running.").end();
