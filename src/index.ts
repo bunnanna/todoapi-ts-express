6d8bfb42fe56daf9901790f3bf62c14dce3be8ee
bunnana <na.nuttanont@gmail.com>
2024-02-05 22:51:18 +0700

    feat: :tada: init project

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { config } from "dotenv";
+import express from "express";
+import "express-async-errors";
+
+config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.status(200).send("Server is running.").end();
+});
+
+app.listen(PORT, () => console.log(`Server running at ${PORT}`));
