ba5951a1c791728eebabdb483a96b65e5371fa79
bunnana <na.nuttanont@gmail.com>
2024-02-06 01:14:09 +0700

    add login dto

--- src/dto/user.ts
+++ src/dto/user.ts
@@ -2,3 +2,4 @@ import { User } from "@prisma/client";
 
 export interface ReqUserDTO extends User {}
 export interface ResUserDTO extends Omit<User, "password"> {}
+export interface LoginDTO extends Pick<User, "email" | "password"> {}
