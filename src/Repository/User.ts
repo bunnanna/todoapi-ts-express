3f7c5bde7378a4a9839fd94a63f36df4d982308c
bunnana <na.nuttanont@gmail.com>
2024-02-06 00:58:10 +0700

    fix naming

--- src/Repository/User.ts
+++ src/Repository/User.ts
@@ -9,33 +9,34 @@ const USER_SELECT = {
 	email: true,
 };
 
-type IUserRepository = IRepository<User, Omit<User, "password">>;
+export interface IUserRepository
+	extends IRepository<User, Omit<User, "password">> {}
 class UserRepository implements IUserRepository {
 	constructor(private user: PrismaClient["user"]) {}
-	GetAll: IUserRepository["GetAll"] = async () => {
+	getAll: IUserRepository["getAll"] = async () => {
 		return this.user.findMany({
 			select: USER_SELECT,
 		});
 	};
 
-	GetOneById: IUserRepository["GetOneById"] = (userId) => {
+	getOneById: IUserRepository["getOneById"] = (userId) => {
 		return this.user.findUniqueOrThrow({
 			where: { userId },
 			select: USER_SELECT,
 		});
 	};
-	Create: IUserRepository["Create"] = async (CreateBody) => {
+	create: IUserRepository["create"] = async (CreateBody) => {
 		await this.user.create({
 			data: CreateBody,
 		});
 	};
-	Update: IUserRepository["Update"] = async (userId, UpdateBody) => {
+	update: IUserRepository["update"] = async (userId, UpdateBody) => {
 		this.user.update({
 			data: UpdateBody,
 			where: { userId },
 		});
 	};
-	Delete: IUserRepository["Delete"] = async (userId) => {
+	delete: IUserRepository["delete"] = async (userId) => {
 		this.user.delete({
 			where: { userId },
 		});
