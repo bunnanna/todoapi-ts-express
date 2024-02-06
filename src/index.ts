686a2dd5ac247e3534fdf7881419a88e0e28b366
bunnana <na.nuttanont@gmail.com>
2024-02-06 19:06:43 +0700

    add logger

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import cookieParser from "cookie-parser";
 import { config } from "dotenv";
 import express from "express";
 import "express-async-errors";
+import morgan from "morgan";
 import { jwtMiddleware } from "./Middlewares/JwtMiddleware";
 import userRouter from "./Routers/User";
 
@@ -12,6 +13,9 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+	morgan(":method :url :status :res[content-length] - :response-time ms")
+);
 app.use(jwtMiddleware.decodeJWT);
 
 app.get("/", (req, res) => {
