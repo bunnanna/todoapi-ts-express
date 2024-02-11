141af09cc854c67890ad852567054ad3e9535915
bunnana <na.nuttanont@gmail.com>
2024-02-12 01:06:57 +0700

    fix dto user readable

--- src/dto/user.ts
+++ src/dto/user.ts
@@ -1,5 +1,16 @@
-import { User } from "@prisma/client";
-
-export interface ReqUserDTO extends Omit<User, "userId"> {}
-export interface ResUserDTO extends Omit<User, "password"> {}
-export interface LoginDTO extends Pick<User, "email" | "password"> {}
+export interface ReqUserDTO {
+	name: string;
+	email: string;
+	password: string;
+	image: string | null;
+}
+export interface ResUserDTO {
+	userId: string;
+	name: string;
+	email: string;
+	image: string | null;
+}
+export interface LoginDTO {
+	email: string;
+	password: string;
+}
